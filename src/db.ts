9bfe9be5ff24e009e75adecdedb3eb392affda96
Brian <bcllui912@gmail.com>
2024-09-20 11:58:11 -0700

    Adding cross-env package for the front end, so we can use a different port without the need to specify a .env file in Windows environments. Verified that both frontend and backend updates starts up.

--- src/db.ts
+++ src/db.ts
@@ -3,18 +3,9 @@
 import mongoose from "mongoose";
 
 // Connect to MongoDB
-mongoose.connect("mongodb://localhost:27017/carbonFootprintDB", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-// Event handlers for the MongoDB connection
-mongoose.connection.on("connected", () => {
-  console.log("MongoDB connected");
-});
-
-mongoose.connection.on("error", (err) => {
-  console.log(`MongoDB connection error: ${err}`);
-});
+mongoose
+  .connect("mongodb://localhost:27017/carbonFootprintDB")
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(`MongoDB connection error: ${err}`));
 
 export default mongoose;
